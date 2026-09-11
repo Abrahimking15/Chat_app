dc27baea02c60124cf69b072b1c6057d12ab398e
Abrahimking15 <m8dhzyyz68@privaterelay.appleid.com>
2026-09-11 15:26:30 +0530

    Update website

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,8 +2,18 @@ import "dotenv/config";
 import express from "express";
 import User from "./models/user.model.js";
 import connectdb from "./lib/db.js";
+import {clerkMiddleware} from '@clerk/express'
+import cors from "cors"
 
 const app = express();
+const FRONTEND_URL=process.env.FRONTEND_URL;
+app.use(clerkMiddleware())
+app.use(cors({origin:FRONTEND_URL,credentials:true}))
+app.use(express.json())
+app.get("/health",(req,res)=>{
+    
+    res.status(200).json({ok:true});
+});
 
 app.listen(3000, () => {
     connectdb();
